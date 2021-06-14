2ce7b5c80665f9eb85783c7aea96befe92efd912
Hussein Ayoub <Hussein-Ayoub-96@hotmail.com>
2021-06-14 18:54:28 +0300

    step 3

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ app.get('/', (req, res) => {
   res.send('ok')
 })
 
+app.get('/test', (req, res) => {
+    res.send({status:200,  message:"ok"})
+  })
+
+
+  app.get('/time', (req, res) => {
+    let date_ob = new Date();
+    let hours = date_ob.getHours();
+    let minutes = date_ob.getMinutes();
+    res.send({status:200, message:hours+":"+minutes})
+   
+  })
+
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
