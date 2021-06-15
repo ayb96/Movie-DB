0697fc9f044b45f9ee48bfd9ead9873ec03aeaef
Hussein Ayoub <Hussein-Ayoub-96@hotmail.com>
2021-06-15 11:25:31 +0300

    step 5

--- index.js
+++ index.js
@@ -37,6 +37,35 @@ app.get('/search', (req, res) => {
   res.send({status:200, message:"ok", data:search});
 })
 
+//step 5
+
+const movies = [
+  { title: 'Jaws', year: 1975, rating: 8 },
+  { title: 'Avatar', year: 2009, rating: 7.8 },
+  { title: 'Brazil', year: 1985, rating: 8 },
+  { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+];
+
+
+app.get('/movies/create', (req, res) => {
+  res.send({status:200,  message:"ok"})
+})
+
+app.get('/movies/read', (req, res) => {
+  res.send({status:200, data: movies})
+})
+
+app.get('/movies/update', (req, res) => {
+  res.send({status:200,  message:"ok"})
+})
+
+app.get('/movies/delete', (req, res) => {
+  res.send({status:200,  message:"ok"})
+})
+
+
+
+
 
 
 app.listen(port, () => {
